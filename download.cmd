6961bb88df97ad1c4fa0c33bc53e63b5b65a2492
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-10-10 20:12:21 +0100

    Change how updates work

---
+++ download.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+:: This is executed by update.cmd
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/SFExt.js
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf16.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf32.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf48.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf128.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/manifest.json
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/update.cmd
