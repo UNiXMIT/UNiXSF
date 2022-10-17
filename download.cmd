4e1c4de1abc067bdd2886baa1a9949f4bd7d7379
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-10-17 16:37:33 +0100

    Updates

--- download.cmd
+++ download.cmd
@@ -1,13 +1,17 @@
 @ECHO OFF
 :: This is executed by update.cmd
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/SFExt.js
-curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf16.png
-curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf32.png
-curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf48.png
-curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/mf128.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/icons/mf16.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/icons/mf32.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/icons/mf48.png
+curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/icons/mf128.png
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/manifest.json
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/update.cmd
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/config.html
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/config.js
 curl -s -O https://raw.githubusercontent.com/UNiXMIT/UNiXSF/main/config.css
+del mf16.png
+del mf32.png
+del mf48.png
+del mf128.png
 
