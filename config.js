4753fbddfda8f9ea30c538eccc8158ffe89c1c6b
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-10-18 21:43:42 +0100

    Updates

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Saves options to chrome.storage
 function save_options() {
     var refreshTimeout = document.getElementById('timeout').value;
     var products = document.getElementById('products').value;
@@ -6,7 +5,6 @@ function save_options() {
       savedTimeout: refreshTimeout,
       savedProducts: products
     }, function() {
-      // Update status to let user know options were saved.
       var status = document.getElementById('status');
       status.textContent = 'Options Saved';
       setTimeout(function() {
@@ -15,7 +13,6 @@ function save_options() {
     });
   }
 
-  // Delete saved options in chrome.storage
   function reset_options() {
     chrome.storage.sync.remove(["savedTimeout","savedProducts"],function(){
       var error = chrome.runtime.lastError;
@@ -26,10 +23,7 @@ function save_options() {
      window.location.reload();
   }
   
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
   function restore_options() {
-    // Use default value
     chrome.storage.sync.get({
         savedTimeout: 60,
         savedProducts: '{"ACUCOBOL-GT (Extend)":"extend-acucobol","Enterprise Developer / Server / Test Server":"enterprise-developer","Visual COBOL":"visual-cobol","Net Express / Server Express":"net-express","Enterprise Analyzer":"enterprise-analyzer","COBOL Analyzer":"cobol-analyzer","COBOL-IT":"cobol-it-ds","RM/COBOL":"rm-cobol","Relativity":"relativity","Data Express":"dataexpress"}',
