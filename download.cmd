6ea06da92021a8b937c964b84b802e36bbf6fa98
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-11-09 16:11:38 +0000

    2.4

--- download.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-ECHO.
-ECHO The Salesforce extension is no longer updated using the update.cmd file. Updates are now downloaded from the Latest Release page as a .zip archive - https://bit.ly/3sEvWxP
-timeout /t 30
