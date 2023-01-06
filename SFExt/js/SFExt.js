86af580bd7b745782867a9349a3576204f598458
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-01-06 08:32:46 +0000

    2.9.1

--- SFExt/js/SFExt.js
+++ SFExt/js/SFExt.js
@@ -959,7 +959,7 @@ function updateCheck() {
         if (xmlHttp.readyState === 4) {
             if (xmlHttp.status === 200) {
                 let latestVersion = xmlHttp.response;
-                let newVersion = compareVersions(installedVersion, latestVersion)
+                let newVersion = compareVersions(installedVersion, latestVersion.toString())
                 if ( (newVersion == 1) ) {
                     (async() => {
                         let updateMessage = `Version ${latestVersion}`;
