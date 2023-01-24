6057e2099ea6d8cbe6090cbeabc07bc15caff0d6
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-01-24 09:52:35 +0000

    2.9.2

--- createArchive.cmd
+++ createArchive.cmd
@@ -3,5 +3,4 @@ set "PATH=C:\Program Files\7-Zip;%PATH%"
 del SFExt.zip
 :: powershell Compress-Archive \GitHub\UNiXSF\SFExt\* \GitHub\UNiXSF\SFExt.zip
 7z a SFExt.zip .\SFExt\*
-7z a SFExt.zip README.md
-7z a SFExt.zip images\
+7z a SFExt.zip README.pdf
