0e5f4d0cc651fe06a96e5770d7058713d2c26a7d
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2025-12-01 18:38:10 +0000

    4.1.4

---
+++ build.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+SET OPTS=%1
+
+FOR /F %%V IN ('jq -r ".version" "SFExtFF\manifest.json"') DO SET "VER=%%V"
+
+IF "%OPTS%"=="submit" GOTO :SUBMIT
+
+:BUILD
+MKDIR updates\FF\%VER%
+CALL web-ext build --overwrite-dest --filename SFExt-%VER%.zip --artifacts-dir . --source-dir .\SFExt\
+MKDIR updates\Chromium\%VER%
+MOVE SFExt-%VER%.zip updates\Chromium\%VER%\
+GOTO :END
+
+:SUBMIT
+web-ext sign --source-dir ./SFExtFF/ --channel unlisted
+GOTO :END
+
+:END
+PAUSE
